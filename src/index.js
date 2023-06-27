47cd1ed1ff9fff15e01e0925a284266fc2096780
Hemalatha <hemalathalekkala@gamil.com>
2023-06-27 14:41:33 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import generateJoke from "./generateJoke";
+import './styles/main.scss';
+import laughing from './assets/laughing.png';
+
+const laughImg = document.getElementById('laughImg');
+laughImg.src = laughing;
+
+const jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener("click", generateJoke);
+
+console.log(generateJoke());
+
